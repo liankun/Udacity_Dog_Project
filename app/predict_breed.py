6c75e2f819dc09f3d29e9946a0fb6251e55543fd
Liankun <zz.guku@gmail.com>
2019-08-19 21:33:34 -0700

    finish the argparse part

--- app/predict_breed.py
+++ app/predict_breed.py
@@ -1,19 +1,71 @@
 import argparse
 
-
 parser = argparse.ArgumentParser()
 
-parser.add_argument('input',action='store',help='image path')
-parser.add_argument('checkpoint',action='store',help='model checkpoint')
-parser.add_argument('--top_k',action='store',default=5,help='top k prediction',type=int)
-parser.add_argument('--category_names',action='store',default=None,help='category names file')
-parser.add_argument('--gpu',action='store_true',default=False,help='enable gpu')
+parser.add_argument('input',
+              action='store',
+              help='image path')
+parser.add_argument('--checkpoint',
+              action='store',
+              default='../saved_models',
+              help='model checkpoint')
+parser.add_argument('--training',
+              action='store_true',
+              default=False,
+              help='retrain the model,need train,test images files or bottleneck features path')
+parser.add_argument('--train_file_path',
+              action='store',
+              default='../dogImages',
+              help='files path to train the model')
+parser.add_argument('--bottleneck_feature_path',
+              action='store',
+              default='../bottleneck_features',
+              help='bottleneck feature path')
+parser.add_argument('--face_detector',
+              action='store',
+              default='cv2',
+              help='choose one of the three face detectors: cv2 hog cnn')
+parser.add_argument('--arch',
+              action='store',
+              default='Xception',
+              help='choose one of the four network arch: VGG19 ResNet50 Inception Xception')
+parser.add_argument('--augmentation',
+              action='store_true',
+              default=False,
+              help='Use augmentation to train the model, warning: bottleneck features will not be used')
+parser.add_argument('--use_bottleneck_feature',
+              action='store_false',
+              default=True,
+              help='Use bottleneck features,False means the image will go from the beginning of the network')
+parser.add_argument('--hidden_layer_nodes',
+              action='store',
+              default=128,
+              help='number of nodes for the hidden layers',
+              type=int)
+parser.add_argument('--free_first_layers',
+              action='store',
+              default=0,
+              help='free parameters of the first n layers for training',
+              type=int)
+parser.add_argument('--free_last_layers',
+              action='store',
+              default=0,
+              help='free parameters of the last n layers for training',
+              type=int)
 parser.add_argument('--verbose',action='store_true',default=False)
 
 result = parser.parse_args()
 if result.verbose:
-    print("image:          {!r}".format(result.input))
-    print("checkpoint:     {!r}".format(result.checkpoint))
-    print("top k :         {!r}".format(result.top_k))
-    print("category names: {!r}".format(result.category_names))
-    print("enable gpu:     {!r}".format(result.gpu))
+    print("image path:           {!r}".format(result.input))
+    print("checkpoint:           {!r}".format(result.checkpoint))
+    print("training :            {!r}".format(result.training))
+    print("train file path :       {!r}".format(result.train_file_path))
+    print("bottleneck features path:  {!r}".format(result.bottleneck_feature_path))
+    print("face detector:         {!r}".format(result.face_detector))
+    print("arch:               {!r}".format(result.arch))
+    print("augmentation:          {!r}".format(result.augmentation))
+    print("use bottleneck feature:   {!r}".format(result.use_bottleneck_feature))
+    print("hidden layer nodes:      {!r}".format(result.hidden_layer_nodes))
+    print("free first n layers:     {!r}".format(result.free_first_layers))
+    print("free last n layers:      {!r}".format(result.free_last_layers))
+    
